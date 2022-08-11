ddc89fb4010d1d5217c371345e6c54304a2d4ef2
Ahmad Fathoni <ahmad.fathoni50@gmail.com>
2022-08-12 00:06:32 +0700

    add summary card

---
+++ src/components/SummaryCard.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const SummaryCard = ({item}) => {
+    return (
+        <div className="w-full flex flex-col max-w-sm px-5 py-4 border-2 rounded-xl shadow-md m-4 bg-white">
+            <h1 className="text-3xl font-semibold text-center">621</h1>
+            <h1 className="text-lg font-semibold text-center">Students</h1>
+        </div>
+    )
+}
