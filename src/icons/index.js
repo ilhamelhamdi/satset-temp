870b8c05416640d0b1a0fa03043ecd2dc72050bd
ilhamelhamdi <ilhamabdillah123@gmail.com>
2022-08-15 19:30:43 +0700

    copy images to src (move planned)

--- src/icons/index.js
+++
@@ -1,9 +0,0 @@
-import { ReactComponent as Search } from './search.svg'
-import { ReactComponent as Menu } from './menu.svg'
-
-const Icons = {
-  Search,
-  Menu
-}
-
-export default Icons
---
+++ src/images/index.js
@@ -0,0 +1,25 @@
+import Concept from './concept.jpg'
+import CourseBanner from './course-banner.png'
+import DefaultUser from './default-user.png'
+import Lectures from './lectures.jpg'
+import Limitless from './limitless.jpg'
+import Professional from './professional.jpg'
+import Quiz from './quiz.jpg'
+import Thinking from './thinking.jpg'
+import Webinar from './webinar.jpg'
+import Logo from './logo.png'
+
+const Images = {
+  Concept,
+  CourseBanner,
+  DefaultUser,
+  Lectures,
+  Limitless,
+  Professional,
+  Quiz,
+  Thinking,
+  Webinar,
+  Logo
+}
+
+export default Images
