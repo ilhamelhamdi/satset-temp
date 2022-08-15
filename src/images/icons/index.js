0d13b871dda7145aacc9e01c0175986322bd444c
ilhamelhamdi <ilhamabdillah123@gmail.com>
2022-08-15 19:31:53 +0700

    copy image to src

---
+++ src/images/icons/index.js
@@ -0,0 +1,15 @@
+import { ReactComponent as Search } from './search.svg'
+import { ReactComponent as Menu } from './menu.svg'
+import { ReactComponent as AddImg } from './add-img.svg'
+import { ReactComponent as Delete } from './delete.svg'
+import { ReactComponent as Dropdown } from './dropdown.svg'
+
+const Icons = {
+  Search,
+  Menu,
+  AddImg,
+  Delete,
+  Dropdown
+}
+
+export default Icons
