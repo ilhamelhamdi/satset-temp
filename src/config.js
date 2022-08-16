9baee7b72eb31adeadb15225e76226d7522d1a94
ilhamelhamdi <ilhamabdillah123@gmail.com>
2022-08-16 23:26:37 +0700

    add config

---
+++ src/config.js
@@ -0,0 +1,6 @@
+const PROXY_URL = 'https://cors-proxy.ilhamabdillah123.workers.dev/?'
+// const PROXY_URL = ''
+const API_URL = PROXY_URL + 'https://satset-mooc.herokuapp.com/api'
+
+
+export { API_URL, PROXY_URL }
