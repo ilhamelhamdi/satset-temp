bb641dea95d4a0e0e0eda35f8deea0c91e4c140d
ilhamelhamdi <ilhamabdillah123@gmail.com>
2022-08-20 23:19:06 +0700

    add skeleton box for content

---
+++ src/components/CreateCourse/ContentBoxSkeleton.js
@@ -0,0 +1,9 @@
+const ContentBoxSkeleton = () => {
+  return (
+    <div className="shadow-lg border-2 border-solid border-slate-200 p-4 mb-4 rounded-lg animate-pulse">
+      <div className="w-full h-8 bg-slate-300" />
+    </div>
+  )
+}
+
+export default ContentBoxSkeleton
